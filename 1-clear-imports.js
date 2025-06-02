e493f005db11c4419ddc477d6c11b3c0e2111fd5
Pavel Romanov <romanovp8913@gmail.com>
2025-06-02 15:50:48 +0700

    Update clear imports example

---
+++ 1-clear-imports.js
@@ -0,0 +1,12 @@
+// Now it's clear which are the built-in ones
+
+import readline from "node:readline";
+import tty from "node:tty";
+import path from "node:path";
+import crypto from "node:crypto";
+import lodash from "lodash";
+import url from "node:url";
+import axios from "axios";
+import os from "node;
+
+console.log('Built-in modules are now obvious!');
